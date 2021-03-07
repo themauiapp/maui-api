165c7d69232116c7e1e56c6c23cc93aa3a155388
Fambegbe Olamileke <olamileke.f@gmail.com>
2021-03-07 22:53:59 +0100

    created initial database structure and other setup

---
+++ database/migrations/2021_03_07_122912_create_income_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncomeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('income', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+            ->constrained('users')
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
+            $table->foreignId('period_id')
+            ->constrained('periods')
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
+            $table->double('total');
+            $table->double('remainder');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('income', function(Blueprint $table) {
+            $table->dropForeign(['user_id', 'period_id']);
+        });
+        Schema::dropIfExists('income');
+    }
+}
---
+++ database/migrations/2021_03_07_123754_create_expenses_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+            ->constrained('users')
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
+            $table->foreignId('income_id')
+            ->constrained('income')
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
+            $table->string('name');
+            $table->double('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('expenses', function(Blueprint $table) {
+            $table->dropForeign(['user_id', 'income_id']);
+        });
+        Schema::dropIfExists('expenses');
+    }
+}
