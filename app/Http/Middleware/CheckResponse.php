db6928224a8edc4a1566ef693ea4530f33a44440
Fambegbe Olamileke <olamileke.f@gmail.com>
2021-06-29 14:05:36 +0100

    fix(auth): delete app cookie when its not valid anymore

---
+++ app/Http/Middleware/CheckResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+
+class CheckResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+        
+        if($response->status() === 419) {
+            Auth::logout();
+            return $response->withoutCookie('maui_cookie');
+        }
+
+        return $response;
+    }
+}
